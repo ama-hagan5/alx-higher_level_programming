a7e8b1e3c9fdd2b4b6cfcb6bf4e3d61085e1c3bd
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-06-29 14:27:08 -0700

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    print("{:0>2d}".format(x), end=", ")
-
-print(x)
+for w in range(0, 100):
+    if w == 99:
+        print("{}".format(w))
+        continue
+    print("{:02}, ".format(w), end="")
