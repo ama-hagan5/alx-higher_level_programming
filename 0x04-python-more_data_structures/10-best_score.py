4666170873485ce8ba32b2a1d86d8ec814325322
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-07-06 15:11:36 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-ef best_score(a_dictionary):
+def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
