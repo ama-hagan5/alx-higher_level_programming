1a56c9cf390fd68ec2bf94d0ad640b406abdfd50
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-07-19 12:06:54 -0700

    Task 5

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                               3,1           All
+
